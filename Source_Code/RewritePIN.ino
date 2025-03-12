c922e12591e7ef5193f76c9a0e3bb7767d8b7870
Luong Minh Nhat <luongminhnhat1604@gmail.com>
2025-03-12 15:14:39 +0700

    Update RewritePIN.ino

--- Source_Code/RewritePIN.ino
+++ Source_Code/RewritePIN.ino
@@ -6,7 +6,7 @@ char defaultPin[5] = "1234";
 void setup() {
   Serial.begin(9600);
   EEPROM.put(PIN_ADDRESS, defaultPin);
-  Serial.println("Mã PIN đã được reset về 1234!");
+  Serial.println("PIN has been reseted to 1234!");
 }
 
 void loop() {
